c4a07a1aa20b60b52996b83467a649815fedb20c
stask9688 <stask9688@yahoo.com>
2017-06-12 19:23:43 -0500

    Added Project model, added classes to view models in admin view

--- timekeeper/admin.py
+++ timekeeper/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, Client
 
+
+class ProjectDetail(admin.ModelAdmin):
+    list_display = ("project_name", "project_description")
+
+
+class ClientDetail(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "email", "phone_number")
+
+
+admin.site.register(Project, ProjectDetail)
+admin.site.register(Client, ClientDetail)
 # Register your models here.
--- timekeeper/models.py
+++ timekeeper/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Client(models.Model):
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField()
     phone_number = models.IntegerField(10)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=30)
+    project_description = models.TextField(max_length=200)
+
