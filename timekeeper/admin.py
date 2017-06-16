cb75384c4b5e4e9d5accb703a8490b6018209216
stask9688 <stask9688@yahoo.com>
2017-06-15 19:34:02 -0500

    Modified models to reference foreign key values

--- timekeeper/admin.py
+++ timekeeper/admin.py
@@ -9,6 +9,7 @@ class ProjectDetail(admin.ModelAdmin):
 class ClientDetail(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "email", "phone_number")
 
+
 class TimecardDetail(admin.ModelAdmin):
     list_display = ("timecard_owner", "timecard_project",
                     "timecard_date", "timecard_hours")
--- timekeeper/models.py
+++ timekeeper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,24 +9,26 @@ class Client(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=14)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Project(models.Model):
     project_name = models.CharField(max_length=30)
     project_description = models.TextField(max_length=200)
+    project_hours = models.IntegerField(default=0)
+    client = models.ForeignKey(Client, null=True)
+
+    def __str__(self):
+        return self.project_name
+
 
 # Timecard object contains the owner(a.k.a current user),
 # the project of the timecard, the date of the timecard,
 # and the amount of hours worked. Create instantiates
 # the timecard object.
 class Timecard(models.Model):
-    timecard_owner = models.CharField(max_length=50)
-    timecard_project = models.CharField(max_length=50)
+    timecard_owner = models.ForeignKey(User, null=True)
+    timecard_project = models.ForeignKey(Project, null=True)
     timecard_date = models.DateField()
-    timecard_hours = models.IntegerField()
-
-    @classmethod
-    def create(cls, o, p, d, h):
-        timecard_temp = cls(timecard_owner=o, timecard_project=p,
-                            timecard_date=d, timecard_hours=h)
-        return timecard_temp
-
+    timecard_hours = models.IntegerField(default=0)
