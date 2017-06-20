6e26e07b31054d9985020b3e634e0c442fa1211d
stask9688 <stask9688@yahoo.com>
2017-06-19 19:25:24 -0500

    Fixed alignment issues where sidebar would overlap content on smaller screens, general small device improvements

--- timekeeper/static/js/ajax_requests.js
+++ timekeeper/static/js/ajax_requests.js
@@ -32,7 +32,7 @@ class AjaxRequest {
 
             //Initialize table chart to be rendered on component with id=project_table
             let tableChart = dc.dataTable("#project_table");
-            tableChart.order(d3.ascending).width(768).height(480)
+            tableChart.order(d3.ascending)
                 .dimension(project_filter.dimension["pk"])
                 .group(function (d) {
                     //Tables don't need groups like other charts,
@@ -137,7 +137,7 @@ class AjaxRequest {
 
             let tableChart = dc.dataTable("#timecard_table");
             timecard_filter.ndx.groupAll();
-            tableChart.order(d3.ascending).width(768).height(480)
+            tableChart.order(d3.ascending)
                 .dimension(timecard_filter.dimension["timecard_date"])
                 .group(function (d) {
                     return d.timecard_date;
@@ -154,8 +154,9 @@ class AjaxRequest {
                         return d.timecard_hours;
                     }
                 }]);
-
+            let height = $(".col-md-12 > form").height();
             tableChart.render();
+            $("#timecard_table").height(height);
             let barChart = dc.barChart("#timecard_graph");
             let hoursGroup = timecard_filter.dimension["timecard_date"].group().reduceSum(
                 function (d) {
@@ -174,7 +175,7 @@ class AjaxRequest {
                 .xAxisLabel('Date')
                 .yAxisLabel('Hours Worked')
                 .dimension(timecard_filter.dimension["timecard_date"])
-                .group(hoursGroup).width(768).height(480);
+                .group(hoursGroup);
             barChart.render()
 
 
